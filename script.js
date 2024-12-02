4ba9e91630f7148aab200b46b312419ef9ca3292
Johnrxyz <johnreymansilungan122@gmail.com>
2024-12-03 00:49:43 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSidebar(){
+    const phonebar = document.querySelector('.phonebar');
+    phonebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+    const phonebar = document.querySelector('.phonebar');
+    phonebar.style.display = 'none'
+}
